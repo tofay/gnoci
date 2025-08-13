1619dd458985e18260add5af9281b08163cf2f96
Tom Fay <tom@teamfay.co.uk>
2025-08-13 12:54:36 +0100

    fix: improve error when entry file not found (#19)

--- src/lib.rs
+++ src/lib.rs
@@ -468,11 +468,11 @@ impl<'a> ImageBuilder<'a> {
 /// If the `SOURCE_DATE_EPOCH` environment variable is set, it will use that as the creation time.
 #[must_use]
 pub fn creation_time() -> chrono::DateTime<chrono::Utc> {
-    if let Ok(epoch) = std::env::var("SOURCE_DATE_EPOCH") {
-        if let Ok(epoch) = epoch.parse::<i64>() {
-            return chrono::DateTime::<chrono::Utc>::from_timestamp(epoch, 0)
-                .unwrap_or(chrono::Utc::now());
-        }
+    if let Ok(epoch) = std::env::var("SOURCE_DATE_EPOCH")
+        && let Ok(epoch) = epoch.parse::<i64>()
+    {
+        return chrono::DateTime::<chrono::Utc>::from_timestamp(epoch, 0)
+            .unwrap_or(chrono::Utc::now());
     }
     chrono::Utc::now()
 }
@@ -577,12 +577,19 @@ impl<W: Write> LayerBuilder<W> {
 }
 
 fn write_entry(builder: &mut tar::Builder<impl Write>, entry: &Entry) -> Result<()> {
-    // Add xattrs if required
-    builder.append_xattr_header(&entry.source)?;
-
     // Don't use symlink metadata, as we want to follow the symlink
     // and add the actual file or directory.
-    let metadata = fs::metadata(&entry.source)?;
+    let metadata = fs::metadata(&entry.source).context(format!(
+        "Failed to read metadata of {}",
+        entry.source.display()
+    ))?;
+
+    // Add xattrs if required
+    builder.append_xattr_header(&entry.source).context(format!(
+        "Failed to append xattrs for {}",
+        entry.source.display()
+    ))?;
+
     let target = entry.relative_target_path();
     let mut header = Header::new_gnu();
     header.set_metadata_in_mode(&metadata, tar::HeaderMode::Deterministic);
